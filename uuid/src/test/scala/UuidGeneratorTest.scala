dcf2f64a2d383102453e9b62fbec0436e19b303f
Jakub Wojnowski <29680262+jwojnowski@users.noreply.github.com>
2024-04-02 02:22:16 +0200

    💚 Fix UUID module

--- uuid/src/test/scala/UuidGeneratorTest.scala
+++ uuid/src/test/scala/UuidGeneratorTest.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2023 Jakub Wojnowski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import cats.effect.std.UUIDGen
 import me.wojnowski.humanoid.HumanId
 import me.wojnowski.humanoid.HumanIdOps
@@ -14,7 +35,7 @@ class UuidGeneratorTest extends FunSuite {
 
   val uuid1 = UUID.fromString("076831d6-41a0-4161-9217-fc04ac8fa5a2")
 
-  implicit val constTryUuidGen = new UUIDGen[Try] {
+  implicit val constTryUuidGen: UUIDGen[Try] = new UUIDGen[Try] {
     override def randomUUID: Try[UUID] = Success(uuid1)
   }
 
